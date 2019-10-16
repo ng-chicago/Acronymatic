7a97c9751215c6d6df41568ef468a420f69c6ee4
ng-chicago <Mathias@ng-chicago.com>
2019-10-16 10:51:04 -0500

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { HomeComponent } from './core/home/home.component';
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'quiz', loadChildren: () => import('./quiz/quiz.module').then(m => m.QuizModule) },
-  { path: 'search', loadChildren: () => import('./search/search.module').then(m => m.SearchModule) },
   { path: '404', loadChildren: () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule) },
   { path: '**', redirectTo: '/404' }
 ];
